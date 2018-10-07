34647d6a91f570060ad79377f1530680139a9da8
ICTER Guru <icterguru@gmail.com>
2018-10-07 15:49:10 -0400

    Updated codes pushed

---
+++ src/dataTypes/ByteEx1.java
@@ -0,0 +1,19 @@
+package dataTypes;
+
+public class ByteEx1{
+	public static void main(String args[]) {
+		
+		System.out.println("byte type min value: " + Byte.MIN_VALUE);
+		System.out.println("byte type max value: " + Byte.MAX_VALUE);
+		System.out.println("byte type size: " + 	Byte.SIZE);
+
+		byte  value1 = 100;  // Legal Assigning
+		byte  value2 = 127; // Legal Assigning
+		
+//		byte sum = value1 + value2;
+		
+		System.out.println("Value1: " +value1);
+		System.out.println("Value2: " +value2);
+
+	}
+}
